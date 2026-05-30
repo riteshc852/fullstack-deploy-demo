60fb785b6457dc0090a6361db792ec22ed4d8335
riteshc852 <24amtics556@gmail.com>
2026-05-30 10:39:02 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState(
+    "message isnt passed from server",
+  );
+  useEffect(() => {
+    fetch("http://localhost:5000/api/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => {
+        console.error(`Error fetching message:`, err);
+      });
+  }, []);
+  return (
+    <>
+      <h1>Frontend is installed</h1>
+      <p> message : {message}</p>
+    </>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+        "http://localhost:5173", "http://localhost:5174" , "http://localhost:3000" ],
+  }),
+);
+
+// API route
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from the server!" });
+});
+
+app.listen(PORT, "0.0.0.0",() => {
+  console.log(`Server is running on port ${PORT}`);
+});
